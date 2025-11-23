87143a5123abdee2fabd589180eaac28d074e629
murali varma <166089588+Muralivarmaa@users.noreply.github.com>
2025-11-24 01:58:52 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,49 @@
+'''
+int (1,-1)
+boolean
+float
+complex
+'''
+
+#a=1
+#b=-22
+#print(type(a),(b)) #in this one TYPE shows which data we are using
+
+#a=True
+#b=False
+#print(type(a),type(b))
+
+#method for checking this method was true or false by using 0 1
+#print(True==1)
+
+#print(False==0)
+
+#float
+#c=5.6
+#print(type(c))
+
+#complex data type
+
+#v=2+5j
+#print(type(v))
+
+#converting data types
+'''
+int()
+float()
+bool()
+str()
+'''
+
+#a=2
+#print(float(a)) #it converts int to float.
+
+#b=9.2
+#print(int(b)) #float to int 
+
+'''
+data lose-->explicit type conversion
+no data lose-->implicit type conversion
+one data type to another data type is called "TYPE CONVERSION".
+'''
+
