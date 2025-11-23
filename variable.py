446381d4e6cdece433b0ccbe28056b0719c21ca8
murali varma <166089588+Muralivarmaa@users.noreply.github.com>
2025-11-24 01:59:11 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,31 @@
+#variables
+
+#syntax was variable name = value
+
+#starting letters should be alphabets & underscore 
+
+#we cannot start with numericals and also with symbols
+
+#and also they are case sensitive
+
+#a=5
+#print(a)
+
+#a,b,c=1
+#print(a,b,c) #it shows error
+
+#a,b,c=1,5,9
+#print(a,b,c)
+
+#c=1,2,3
+#print(c)
+
+#a=b=c=15
+#print(a) #output will 15
+#print(a,b,c) #o/p wil 15 15 15.
+
+#for memory location ID
+#a=5
+#print(id(a)) 
+
+
