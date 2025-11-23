c033b698053176498b385e599650c0a4f3e21dbe
murali varma <166089588+Muralivarmaa@users.noreply.github.com>
2025-11-24 01:58:27 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,86 @@
+'''
+decision making statements
+
+
+if 
+else
+elif
+nested if
+
+'''
+#syntax was 
+'''
+if condition:
+    statements
+else:
+     statements   #if condition false then else condition executes
+     '''
+     
+# if True:     
+#      print("i am if")
+# elif True:
+#         print("iam elif")
+# else:
+#     print("i am else")
+    
+            
+'''
+in above code if condition true then o/p comes
+ootherwise if condition was flase then
+it goes to elif . and suppose if and elif was false
+then it directly goes to else and gives that o/p
+
+'''
+#else has no conditions. 
+
+# if False:
+#     print("outer if")
+#     if False:
+#         print("inner if")
+#     else:
+#         print("inner else")
+# else:
+#     print("outer else")
+    
+    
+#another example.
+
+# age=18
+# if age>18:
+#     print("you can vote")
+# elif age==18:
+#     print("you can also vote")
+# else:
+#     print("wait bro") 
+ 
+ 
+'''          
+by using OR in multiple conditions we dont need 
+to mention elif becoz we can directly give values
+there
+
+'''  
+#for example using OR insted of elif
+
+age=18
+if age>18 or age==18:
+    print("you can vote")
+else:
+    print("wait bro") 
+    
+  # to check multiple conditions we can use the OR , AND logical operators in conditional statements.
+                
+    
+    
+            
+                     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
